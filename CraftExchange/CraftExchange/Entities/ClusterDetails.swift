5505959b6f0d1d424500ad0191eb7002624966ec
Preety Singh <preety.singh@adrosonic.com>
2020-06-02 04:28:23 +0530

    Cluster API Added

--- CraftExchange/CraftExchange/Entities/ClusterDetails.swift
+++ CraftExchange/CraftExchange/Entities/ClusterDetails.swift
@@ -14,10 +14,12 @@ class ClusterDetails: Object, Decodable {
 
     @objc dynamic var entityID: Int = 0
     @objc dynamic var clusterDescription: String?
-
+    let prodCategory = List<ProductCategory>()
+  
     enum CodingKeys: String, CodingKey {
         case id = "id"
         case clusterDescription = "desc"
+        case prodCategory = "productCategory"
     }
 
     convenience required init(from decoder: Decoder) throws {
@@ -25,5 +27,8 @@ class ClusterDetails: Object, Decodable {
       let values = try decoder.container(keyedBy: CodingKeys.self)
       entityID = try (values.decodeIfPresent(Int.self, forKey: .id) ?? 0)
       clusterDescription = try values.decodeIfPresent(String.self, forKey: .clusterDescription)
+      if let prodCategory = try? values.decodeIfPresent([ProductCategory].self, forKey: .prodCategory) {
+          self.prodCategory.append(objectsIn: prodCategory)
+      }
     }
 }
---
+++ CraftExchange/CraftExchange/Entities/Entity+Networking/ClusterDetails+Request.swift
@@ -0,0 +1,52 @@
+//
+//  ClusterDetails+Request.swift
+//  CraftExchange
+//
+//  Created by Preety Singh on 02/06/20.
+//  Copyright © 2020 Adrosonic. All rights reserved.
+//
+
+import Foundation
+import Realm
+import RealmSwift
+
+extension ClusterDetails {
+  
+public static func getAllClusters() -> Request<[ClusterDetails], APIError> {
+    return Request(
+        path: "cluster/getAllClusters",
+        method: .get,
+        resource: { print(String(data: $0, encoding: .utf8) ?? "cluster fetch failed")
+          if let json = try? JSONSerialization.jsonObject(with: $0, options: .allowFragments) as? [String: Any] {
+            if let array = json["data"] as? [[String: Any]] {
+                let data = try JSONSerialization.data(withJSONObject: array, options: .fragmentsAllowed)
+                let object = try JSONDecoder().decode([ClusterDetails].self, from: data)
+                return object
+            }
+          }
+          return []
+    },
+        error: APIError.init,
+        needsAuthorization: false
+    )
+  }
+  
+  public static func getProductsForCluster(clusterId:Int) -> Request<[ClusterDetails], APIError> {
+    return Request(
+        path: "cluster/getProductCategories/\(clusterId)",
+        method: .get,
+        resource: { print(String(data: $0, encoding: .utf8) ?? "cluster product details fetch failed")
+          if let json = try? JSONSerialization.jsonObject(with: $0, options: .allowFragments) as? [String: Any] {
+            if let array = json["data"] as? [[String: Any]] {
+                let data = try JSONSerialization.data(withJSONObject: array, options: .fragmentsAllowed)
+                let object = try JSONDecoder().decode([ClusterDetails].self, from: data)
+                return object
+            }
+          }
+          return []
+    },
+        error: APIError.init,
+        needsAuthorization: false
+    )
+  }
+}
