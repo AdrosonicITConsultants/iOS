cb2c82a7cc5767769882e2bace18ac9bfce5fc2b
Preety Singh <preety.singh@adrosonic.com>
2020-08-28 13:58:12 +0530

    Buyer Product Catalogue Title Font Changed UILabel extension added

---
+++ CraftExchange/CraftExchange/Framework/Utils/Extensions/UILabel+Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  UILabel+Extensions.swift
+//  CraftExchange
+//
+//  Created by Preety Singh on 28/08/20.
+//  Copyright © 2020 Adrosonic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+
+    func addImageWith(name: String, behindText: Bool) {
+
+        let attachment = NSTextAttachment()
+        attachment.image = UIImage(named: name)
+        let lFontSize = round(self.font.pointSize * 1.32)
+        
+        if let image = UIImage(named: name) {
+            let lRatio = image.size.width / image.size.height
+            attachment.bounds = CGRect(x: 0, y: ((self.font.capHeight - lFontSize) / 2).rounded(), width: lRatio * lFontSize, height: lFontSize)
+        }
+
+        let attachmentString = NSAttributedString(attachment: attachment)
+
+        guard let txt = self.text else {
+            return
+        }
+
+        if behindText {
+            let strLabelText = NSMutableAttributedString(string: txt)
+            strLabelText.append(attachmentString)
+            self.attributedText = strLabelText
+        } else {
+            let strLabelText = NSAttributedString(string: txt)
+            let mutableAttachmentString = NSMutableAttributedString(attributedString: attachmentString)
+            mutableAttachmentString.append(strLabelText)
+            self.attributedText = mutableAttachmentString
+        }
+    }
+
+    func removeImage() {
+        let text = self.text
+        self.attributedText = nil
+        self.text = text
+    }
+}
