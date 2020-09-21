a555e8daaba81db5dc7cdcd12bb3a4f38843a3e2
Preety Singh <preety.singh@adrosonic.com>
2020-09-21 18:23:24 +0530

    Image URL updated to be used from Keychain Manager

--- CraftExchange/CraftExchange/View/Controller/ProfileController/ArtisanBrandDetails.swift
+++ CraftExchange/CraftExchange/View/Controller/ProfileController/ArtisanBrandDetails.swift
@@ -231,7 +231,7 @@ class ArtisanBrandDetails: FormViewController, ButtonActionProtocol {
     
     func refreshBrandLogo() {
         if let name = User.loggedIn()?.buyerCompanyDetails.first?.logo, let userID = User.loggedIn()?.entityID {
-            let url = URL(string: "https://f3adac-craft-exchange-resource.objectstore.e2enetworks.net/User/\(userID)/CompanyDetails/Logo/\(name)")
+            let url = URL(string: "\(KeychainManager.standard.imageBaseURL)/User/\(userID)/CompanyDetails/Logo/\(name)")
             URLSession.shared.dataTask(with: url!) { data, response, error in
                 // do your stuff here...
                 DispatchQueue.main.async {
