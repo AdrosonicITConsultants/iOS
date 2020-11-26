707ba5031d565eadb41d78653168e95f99bf84d5
Kalyan Kumar <Kalyan.Kumar@adrosonic.com>
2020-11-27 01:50:47 +0530

    Mark resolved, in progress

---
+++ CraftExchange/CraftExchange/View/CustomAssets/Popups/ResolveConcernView.swift
@@ -0,0 +1,21 @@
+//
+//  ResolveConcernView.swift
+//  CraftExchange
+//
+//  Created by Kalyan on 27/11/20.
+//  Copyright © 2020 Adrosonic. All rights reserved.
+//
+
+import UIKit
+
+class ResolveConcernView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
