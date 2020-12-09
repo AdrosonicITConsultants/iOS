46c4ba3d773a29eadb0b01ee0cc536d5fb2776d3
Preety Singh <preety.singh@adrosonic.com>
2020-12-09 23:56:44 +0530

    LaunchScreen Image Change, Version and Build Number Change, Removed Notification background app listener delegate method

--- CraftExchange/CraftExchange/View/Controller/SidePanel/SideMenuController.swift
+++ CraftExchange/CraftExchange/View/Controller/SidePanel/SideMenuController.swift
@@ -223,7 +223,7 @@ class SideMenuController: FormViewController {
         lbl.textColor = .lightGray
         lbl.textAlignment = .center
         let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-        lbl.text = "23-09-2020 V\(appVersion ?? "1.0")"
+        lbl.text = "09-12-2020 V\(appVersion ?? "1.1")"
         self.view.addSubview(lbl)
         self.view.addSubview(view)
     }
