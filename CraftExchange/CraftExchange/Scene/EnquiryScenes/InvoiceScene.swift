656dd4df84d9c48332585e68a0dfb474a96d5266
Kalyan Kumar <Kalyan.Kumar@adrosonic.com>
2020-12-11 10:54:44 +0530

    Invoice changes

--- CraftExchange/CraftExchange/Scene/EnquiryScenes/InvoiceScene.swift
+++ CraftExchange/CraftExchange/Scene/EnquiryScenes/InvoiceScene.swift
@@ -78,112 +78,19 @@ extension EnquiryDetailsService {
         
         vc.viewModel.sendFI = { (isSave) in
             if vc.viewModel.quantity.value != nil && vc.viewModel.quantity.value?.isNotBlank ?? false && vc.viewModel.pricePerUnitPI.value != nil && vc.viewModel.pricePerUnitPI.value?.isNotBlank ?? false &&  vc.viewModel.finalamount.value != nil && vc.viewModel.finalamount.value?.isNotBlank ?? false &&
-                vc.viewModel.amountToBePaid.value != nil && vc.viewModel.amountToBePaid.value?.isNotBlank ?? false && vc.viewModel.cgst.value != nil && vc.viewModel.cgst.value?.isNotBlank ?? false &&
-                vc.viewModel.sgst.value != nil && vc.viewModel.sgst.value?.isNotBlank ?? false && vc.viewModel.deliveryCharges.value != nil && vc.viewModel.deliveryCharges.value?.isNotBlank ?? false && vc.viewModel.advancePaidAmount.value != nil && vc.viewModel.advancePaidAmount.value?.isNotBlank ?? false && vc.viewModel.previousTotalAmount.value != nil && vc.viewModel.previousTotalAmount.value?.isNotBlank ?? false   {
-                
-                let quantity = vc.viewModel.quantity.value ?? ""
-                let pricePerUnitPI = vc.viewModel.pricePerUnitPI.value ?? ""
-                //                let finalamount = vc.viewModel.finalamount.value ?? ""
-                //                let amountToBePaid = vc.viewModel.amountToBePaid.value ?? ""
-                let cgst = "\(Int(vc.viewModel.cgst.value ?? "0.0") ?? Int(Double(vc.viewModel.cgst.value ?? "0.0") ?? 1000.0))"
-                let sgst = "\(Int(vc.viewModel.sgst.value ?? "0.0") ?? Int(Double(vc.viewModel.sgst.value ?? "0.0") ?? 1000.0))"
-                let deliveryCharges = vc.viewModel.deliveryCharges.value ?? ""
-                let advancePaidAmount = vc.viewModel.advancePaidAmount.value ?? ""
-                let previousTotalAmount = vc.viewModel.previousTotalAmount.value ?? ""
-                let checkBox = vc.viewModel.checkBox.value
-                
-                if checkBox == 1{
-                    if quantity.isValidNumber && Int(vc.viewModel.quantity.value!)! > 0 {
-                        if pricePerUnitPI.isValidNumber && Int(vc.viewModel.pricePerUnitPI.value!)! > 0{
-                            if previousTotalAmount.isValidNumber && Int(vc.viewModel.previousTotalAmount.value!)! > 0 {
-                                if advancePaidAmount.isValidNumber{
-                                    if sgst.isValidNumber && sgst != "1000"{
-                                        if cgst.isValidNumber && cgst != "1000" {
-                                            if deliveryCharges.isValidNumber {
-                                                if isSave == 1{
-                                                    self.previewFinalInvoice(enquiryId: "\(vc.orderObject!.enquiryId)", advancePaidAmount: vc.viewModel.advancePaidAmount.value!, finalTotalAmount: Int(vc.viewModel.finalamount.value ?? "0.0") ?? 0, quantity: Int(vc.viewModel.quantity.value!)!, ppu: Int(vc.viewModel.pricePerUnitPI.value!)!, cgst: vc.viewModel.cgst.value!, sgst: vc.viewModel.sgst.value!, deliveryCharges: vc.viewModel.deliveryCharges.value!).bind(to: vc, context: .global(qos: .background)) {_,responseData in
-                                                        if let json = try? JSONSerialization.jsonObject(with: responseData, options: .allowFragments) as? [String: Any] {
-                                                            if json["valid"] as? Bool == true {
-                                                                DispatchQueue.main.async {
-                                                                    
-                                                                    if let object = json["data"] as? String {
-                                                                        
-                                                                        if vc.orderObject?.orderCode != nil && vc.orderObject?.lastUpdated != nil{
-                                                                            let date = Date().ttceISOString(isoDate: vc.orderObject?.lastUpdated ?? Date())
-                                                                            vc.view.showPreviewPIView(controller: vc, entityId: (vc.orderObject?.orderCode ?? "\(vc.orderObject?.enquiryId ?? 0)"), date: date, data: object, isPI: false)
-                                                                            vc.hideLoading()
-                                                                        }
-                                                                    }
-                                                                    
-                                                                    
-                                                                    vc.hideLoading()
-                                                                }
-                                                            }
-                                                            
-                                                        }
-                                                        
-                                                        
-                                                        
-                                                    }
-                                                    
-                                                }
-                                                if isSave == 2 {
-                                                    self.sendFinalInvoice(enquiryId: "\(vc.orderObject!.enquiryId)", advancePaidAmount: vc.viewModel.advancePaidAmount.value!, finalTotalAmount: Int(vc.viewModel.finalamount.value!)!, quantity: Int(vc.viewModel.quantity.value!)!, ppu: Int(vc.viewModel.pricePerUnitPI.value!)!, cgst: vc.viewModel.cgst.value!, sgst: vc.viewModel.sgst.value!, deliveryCharges: vc.viewModel.deliveryCharges.value!).bind(to: vc, context: .global(qos: .background)) {_,responseData in
-                                                        if let json = try? JSONSerialization.jsonObject(with: responseData, options: .allowFragments) as? [String: Any] {
-                                                            if json["valid"] as? Bool == true {
-                                                                DispatchQueue.main.async {
-                                                                    print("Final Invoice sent successfully")
-                                                                    vc.hideLoading()
-                                                                    vc.navigationController?.popViewController(animated: true)
-                                                                    
-                                                                }
-                                                            }
-                                                            else {
-                                                                vc.alert("Sending final invoice failed, please try again later".localized)
-                                                                vc.hideLoading()
-                                                            }
-                                                        }
-                                                    }.dispose(in: vc.bag)
-                                                }
-                                                
-                                            }else {
-                                                vc.alert("Please enter valid delivery charges".localized)
-                                                vc.hideLoading()
-                                            }
-                                            
-                                        }else {
-                                            vc.alert("Please enter valid CGST")
-                                            vc.hideLoading()
-                                        }
-                                    }else {
-                                        vc.alert("Please enter valid SGST")
-                                        vc.hideLoading()
-                                    }
-                                    
-                                }else {
-                                    vc.alert("Please enter valid previous advance payment received".localized)
-                                    vc.hideLoading()
-                                }
-                            }else {
-                                vc.alert("Please enter valid previous total amount".localized)
-                                vc.hideLoading()
-                            }
-                            
-                            
-                        }else {
-                            vc.alert("Please enter valid price per unit".localized)
-                            vc.hideLoading()
-                        }
+                vc.viewModel.cgst.value != nil && vc.viewModel.cgst.value?.isNotBlank ?? false &&
+                vc.viewModel.sgst.value != nil && vc.viewModel.sgst.value?.isNotBlank ?? false && vc.viewModel.deliveryCharges.value != nil && vc.viewModel.deliveryCharges.value?.isNotBlank ?? false &&  vc.viewModel.previousTotalAmount.value != nil && vc.viewModel.previousTotalAmount.value?.isNotBlank ?? false   {
+                if vc.orderObject?.productStatusId != 2 {
+                    if vc.viewModel.advancePaidAmount.value != nil && vc.viewModel.advancePaidAmount.value?.isNotBlank ?? false && vc.viewModel.amountToBePaid.value != nil && vc.viewModel.amountToBePaid.value?.isNotBlank ?? false {
+                        self.sendPIFunc(Controller: vc, isSave: isSave )
+                        
                     }else {
-                        vc.alert("Please enter valid quantity".localized)
+                        vc.alert("Please enter all mandatory fields".localized)
                         vc.hideLoading()
                     }
-                    
                 }else{
-                    vc.alert("Please Agree to terms& conditions".localized)
-                    vc.hideLoading()
+                    self.sendPIFunc(Controller: vc, isSave: isSave )
                 }
-                
                 
             }else {
                 vc.alert("Please enter all mandatory fields".localized)
@@ -264,5 +171,112 @@ extension EnquiryDetailsService {
             }.dispose(in: vc.bag)
         }
         
+    }
+    
+    func sendPIFunc(Controller: UIViewController, isSave: Int) {
+        if let vc = Controller as? InvoiceController {
+            let quantity = vc.viewModel.quantity.value ?? ""
+            let pricePerUnitPI = vc.viewModel.pricePerUnitPI.value ?? ""
+            //                let finalamount = vc.viewModel.finalamount.value ?? ""
+            //                let amountToBePaid = vc.viewModel.amountToBePaid.value ?? ""
+            let cgst = "\(Int(vc.viewModel.cgst.value ?? "0.0") ?? Int(Double(vc.viewModel.cgst.value ?? "0.0") ?? 1000.0))"
+            let sgst = "\(Int(vc.viewModel.sgst.value ?? "0.0") ?? Int(Double(vc.viewModel.sgst.value ?? "0.0") ?? 1000.0))"
+            let deliveryCharges = vc.viewModel.deliveryCharges.value ?? ""
+            let advancePaidAmount = vc.viewModel.advancePaidAmount.value ?? ""
+            let previousTotalAmount = vc.viewModel.previousTotalAmount.value ?? ""
+            let checkBox = vc.viewModel.checkBox.value
+            
+            if checkBox == 1{
+                if quantity.isValidNumber && Int(vc.viewModel.quantity.value!)! > 0 {
+                    if pricePerUnitPI.isValidNumber && Int(vc.viewModel.pricePerUnitPI.value!)! > 0{
+                        if previousTotalAmount.isValidNumber && Int(vc.viewModel.previousTotalAmount.value!)! > 0 {
+                            if advancePaidAmount.isValidNumber{
+                                if sgst.isValidNumber && sgst != "1000"{
+                                    if cgst.isValidNumber && cgst != "1000" {
+                                        if deliveryCharges.isValidNumber {
+                                            if isSave == 1{
+                                                self.previewFinalInvoice(enquiryId: "\(vc.orderObject!.enquiryId)", advancePaidAmount: vc.viewModel.advancePaidAmount.value!, finalTotalAmount: Int(vc.viewModel.finalamount.value ?? "0.0") ?? 0, quantity: Int(vc.viewModel.quantity.value!)!, ppu: Int(vc.viewModel.pricePerUnitPI.value!)!, cgst: vc.viewModel.cgst.value!, sgst: vc.viewModel.sgst.value!, deliveryCharges: vc.viewModel.deliveryCharges.value!).bind(to: vc, context: .global(qos: .background)) {_,responseData in
+                                                    if let json = try? JSONSerialization.jsonObject(with: responseData, options: .allowFragments) as? [String: Any] {
+                                                        if json["valid"] as? Bool == true {
+                                                            DispatchQueue.main.async {
+                                                                
+                                                                if let object = json["data"] as? String {
+                                                                    
+                                                                    if vc.orderObject?.orderCode != nil && vc.orderObject?.lastUpdated != nil{
+                                                                        let date = Date().ttceISOString(isoDate: vc.orderObject?.lastUpdated ?? Date())
+                                                                        vc.view.showPreviewPIView(controller: vc, entityId: (vc.orderObject?.orderCode ?? "\(vc.orderObject?.enquiryId ?? 0)"), date: date, data: object, isPI: false)
+                                                                        vc.hideLoading()
+                                                                    }
+                                                                }
+                                                                
+                                                                
+                                                                vc.hideLoading()
+                                                            }
+                                                        }
+                                                        
+                                                    }
+                                                    
+                                                    
+                                                    
+                                                }
+                                                
+                                            }
+                                            if isSave == 2 {
+                                                self.sendFinalInvoice(enquiryId: "\(vc.orderObject!.enquiryId)", advancePaidAmount: vc.viewModel.advancePaidAmount.value!, finalTotalAmount: Int(vc.viewModel.finalamount.value!)!, quantity: Int(vc.viewModel.quantity.value!)!, ppu: Int(vc.viewModel.pricePerUnitPI.value!)!, cgst: vc.viewModel.cgst.value!, sgst: vc.viewModel.sgst.value!, deliveryCharges: vc.viewModel.deliveryCharges.value!).bind(to: vc, context: .global(qos: .background)) {_,responseData in
+                                                    if let json = try? JSONSerialization.jsonObject(with: responseData, options: .allowFragments) as? [String: Any] {
+                                                        if json["valid"] as? Bool == true {
+                                                            DispatchQueue.main.async {
+                                                                print("Final Invoice sent successfully")
+                                                                vc.hideLoading()
+                                                                vc.navigationController?.popViewController(animated: true)
+                                                                
+                                                            }
+                                                        }
+                                                        else {
+                                                            vc.alert("Sending final invoice failed, please try again later".localized)
+                                                            vc.hideLoading()
+                                                        }
+                                                    }
+                                                }.dispose(in: vc.bag)
+                                            }
+                                            
+                                        }else {
+                                            vc.alert("Please enter valid delivery charges".localized)
+                                            vc.hideLoading()
+                                        }
+                                        
+                                    }else {
+                                        vc.alert("Please enter valid CGST")
+                                        vc.hideLoading()
+                                    }
+                                }else {
+                                    vc.alert("Please enter valid SGST")
+                                    vc.hideLoading()
+                                }
+                                
+                            }else {
+                                vc.alert("Please enter valid previous advance payment received".localized)
+                                vc.hideLoading()
+                            }
+                        }else {
+                            vc.alert("Please enter valid previous total amount".localized)
+                            vc.hideLoading()
+                        }
+                        
+                        
+                    }else {
+                        vc.alert("Please enter valid price per unit".localized)
+                        vc.hideLoading()
+                    }
+                }else {
+                    vc.alert("Please enter valid quantity".localized)
+                    vc.hideLoading()
+                }
+                
+            }else{
+                vc.alert("Please Agree to terms& conditions".localized)
+                vc.hideLoading()
+            }
+        }
     }
 }
