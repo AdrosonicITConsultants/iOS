6fa39086e1aed67d4d6704f2ebbc54cc54f65406
Kalyan Kumar <Kalyan.Kumar@adrosonic.com>
2020-12-15 16:45:18 +0530

    CR: Advance Payment flow for Revised PI Completed

---
+++ CraftExchange/CraftExchange/View/Controller/EnquiryController/RevisedPaymentController.swift
@@ -0,0 +1,9 @@
+//
+//  RevisedPaymentController.swift
+//  CraftExchange
+//
+//  Created by Kalyan on 14/12/20.
+//  Copyright © 2020 Adrosonic. All rights reserved.
+//
+
+import Foundation
---
+++ CraftExchange/CraftExchange/View/CustomAssets/EnquiryCells/PendingPaymentView.swift
@@ -0,0 +1,24 @@
+//
+//  PendingPaymentView.swift
+//  CraftExchange
+//
+//  Created by Kalyan on 14/12/20.
+//  Copyright © 2020 Adrosonic. All rights reserved.
+//
+
+import UIKit
+
+class PendingPaymentView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
